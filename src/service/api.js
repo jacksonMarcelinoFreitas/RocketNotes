bd3189332225cbb9ba636e67dbff508438071719
Jackson Freitas <jacksonzitap.mc@gmail.com>
2023-07-18 15:24:18 -0300

    test

--- src/service/api.js
+++ src/service/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: "https://rocketnotes-api-dwl5.onrender.com"
+  // baseURL: "http://localhost:8080"
 });
 
 api.get("/users/:id");
